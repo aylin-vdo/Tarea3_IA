797cbb33ee84451a2e1624156c510e986050311d
Aylin <aylindemetci@gmail.com>
2023-02-23 15:31:48 -0600

    No informado

--- BFS.py
+++ BFS.py
@@ -1,13 +1,32 @@
 from collections import deque
 
-def bfs(grafo, inicio):
-    fila = deque([inicio])
-    visitado = set([inicio])
+class Node:
+    def __init__(self, weight, profit, parent=None):
+        self.weight = weight
+        self.profit = profit
+        self.parent = parent
 
-    while fila:
-        nodo = fila.popleft()
-        for vecino in grafo[nodo]:
-            if vecino not in visitado:
-                visitado.add(vecino)
-                fila.append(vecino)
-    return fila
+def bfs(weights, profits, max_weight):
+    n = len(weights)
+    nodes = deque([Node(weights[0], profits[0])])
+    
+    while nodes:
+        node = nodes.popleft()
+        if node.weight <= max_weight:
+            path = [node]
+            while node.parent:
+                path.append(node.parent)
+                node = node.parent
+            path.reverse()
+            return [p.profit for p in path], [p.weight for p in path], sum(p.profit for p in path)
+        
+        for i in range(1, n):
+            new_weight = node.weight + weights[i]
+            if new_weight > max_weight:
+                continue
+            new_profit = node.profit + profits[i]
+            new_node = Node(new_weight, new_profit, node)
+            nodes.append(new_node)
+    
+    return [], [], 0
+
